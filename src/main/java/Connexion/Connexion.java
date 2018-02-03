c93c4ef4f5a71237b3430db9d138db5b5f52fb1e
Tahiry Mihanta <tahirymihanta@gmail.com>
2018-02-03 15:54:04 +0300

    page ivelany

--- src/main/java/Connexion/Connexion.java
+++ src/main/java/Connexion/Connexion.java
@@ -1,4 +1,4 @@
- /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,20 +9,21 @@ package connexion;
  *
  * @author natha
  */
-
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
+
 /**
  *
  * @author Bae
  */
 public class Connexion {
+
     DB db;
 
     public DB getConnection() throws Exception {
-   // MongoClient mongo = new MongoClient("ds119018.mlab.com", 19018);
-    //return db = mongo.getDB("weplay");
-    MongoClient mongo = new MongoClient("localhost", 27017);
-    return db = mongo.getDB("weplay");
+        MongoClient mongo = new MongoClient("ds119018.mlab.com", 19018);
+        return db = mongo.getDB("weplay");
+        //MongoClient mongo = new MongoClient("localhost", 27017);
+        //return db = mongo.getDB("weplay");
     }
 }
